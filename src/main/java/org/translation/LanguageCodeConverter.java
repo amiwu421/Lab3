d2cc38218e7bfd9427b2adc648f7ed6fd3b2272f
ruizhen <xhruizhen@gmail.com>
2024-09-26 16:57:09 -0400

    LanguageCodeConverter.java passing tests

--- src/main/java/org/translation/LanguageCodeConverter.java
+++ src/main/java/org/translation/LanguageCodeConverter.java
@@ -36,14 +36,14 @@ public class LanguageCodeConverter {
             List<String> lines = Files.readAllLines(Paths.get(getClass()
                     .getClassLoader().getResource(filename).toURI()));
 
-            // TODO Task: use lines to populate the instance variable
-            //           tip: you might find it convenient to create an iterator using lines.iterator()
-            for (String line : lines) {
-                String[] parts = line.split("\t");
-                if (parts.length == 2) {
-                    langToCode.put(parts[0].trim(), parts[1].trim());
-                    codeToLang.put(parts[1].trim(), parts[0].trim());
-                }
+            for (int i = 1; i < lines.size(); i++) {
+                String[] parts = lines.get(i).split("\t");
+                langToCode.put(parts[1],parts[0]);
+                codeToLang.put(parts[0],parts[1]);
+//                if (parts.length == 2) {
+//                    langToCode.put(parts[0].trim(), parts[1].trim());
+//                    codeToLang.put(parts[1].trim(), parts[0].trim());
+//                }
             }
         }
         catch (IOException | URISyntaxException ex) {
@@ -58,8 +58,7 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return codeToLang.get(code);
+        return langToCode.get(code);
     }
 
     /**
@@ -68,8 +67,7 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return langToCode.get(language);
+        return codeToLang.get(language);
     }
 
     /**
@@ -77,7 +75,6 @@ public class LanguageCodeConverter {
      * @return how many languages are included in this code converter.
      */
     public int getNumLanguages() {
-        // TODO Task: update this code to use your instance variable to return the correct value
         return codeToLang.size();
     }
 }
